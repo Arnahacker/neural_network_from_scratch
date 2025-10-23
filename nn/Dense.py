50fa475193c7a07589e943d4fe72235f2f51fbbd
Arnav Chhabra <75874456+Arnahacker@users.noreply.github.com>
2025-10-23 17:05:26 +0530

    accommodating_optimiser

--- nn/Dense.py
+++ nn/Dense.py
@@ -3,16 +3,16 @@ from layers import Layer
 
 class Dense(Layer):
     def __init__(self, input_size, output_size):
-        self.weights = np.random.randn(output_size, input_size)
-        self.bias = np.random.randn(output_size, 1)
+        self.weights = np.random.randn(output_size, input_size) * 0.01
+        self.bias = np.zeros((output_size, 1))
+        self.weights_gradient = None
+        self.bias_gradient = None
 
     def forward(self, input):
         self.input = input
         return np.dot(self.weights, self.input) + self.bias
 
-    def backward(self, output_gradient, learning_rate):
-        weights_gradient = np.dot(output_gradient, self.input.T)
-        input_gradient = np.dot(self.weights.T, output_gradient)
-        self.weights -= learning_rate * weights_gradient
-        self.bias -= learning_rate * output_gradient
-        return input_gradient
+    def backward(self, output_gradient):
+        self.weights_gradient = np.dot(output_gradient, self.input.T)
+        self.bias_gradient = np.sum(output_gradient, axis=1, keepdims=True)
+        return np.dot(self.weights.T, output_gradient)
