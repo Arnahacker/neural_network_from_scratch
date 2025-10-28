dd3cde5c5205d46d29947ca26df6e606f8769cfa
Arnav Chhabra <75874456+Arnahacker@users.noreply.github.com>
2025-10-28 18:00:41 +0530

    Add ReLU activation function implementation

--- nn/activation_function.py
+++ nn/activation_function.py
@@ -12,6 +12,17 @@ class Tanh(Activation):
 
         super().__init__(tanh, tanh_prime)
 
+class ReLU(Activation):
+    def __init__(self):
+        def relu(x):
+            return np.maximum(0, x)
+
+        def relu_prime(x):
+            return np.where(x > 0, 1, 0)
+
+        super().__init__(relu, relu_prime)
+
+
 class Sigmoid(Activation):
     def __init__(self):
         def sigmoid(x):
